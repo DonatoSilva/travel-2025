9a07a82655a8a171be6d89fbda15d33ab888a13f
Donato Silva <figueroajesus2015@hotmail.com>
2025-01-22 17:10:16 -0500

    Squashed commit of the following:
    commit 7cb79dfed804075cefff55d9b70234371ef46ce3
    Author: Donato Silva <figueroajesus2015@hotmail.com>
    Date:   Wed Jan 22 17:03:41 2025 -0500
    
        feat: Improve handling of errors in mail subscription
    
    commit b9810b62e8ecd32f39f5cd9e1b7bda9fa146ef89
    Author: Donato Silva <figueroajesus2015@hotmail.com>
    Date:   Wed Jan 22 16:53:01 2025 -0500
    
        feat: add database integration and subscription API endpoint
    
    commit b878988ff7b2feb0536d25c84c8e7ab092ac7dfb
    Author: Donato Silva <figueroajesus2015@hotmail.com>
    Date:   Wed Jan 22 10:34:57 2025 -0500
    
        feat: update countdown logic, add confetti in invitation section
    
    commit a6be90a237e56a3b8a3b135760e304d4c1044495
    Author: Donato Silva <figueroajesus2015@hotmail.com>
    Date:   Wed Jan 22 10:31:33 2025 -0500
    
        feat: add js-confetti
    
    commit 3118c2f0f3db3d369eada3bc66d025b60b5bb49b
    Author: Donato Silva <figueroajesus2015@hotmail.com>
    Date:   Wed Jan 22 08:42:54 2025 -0500
    
        feat: add support vercel server

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel';
+
+import db from '@astrojs/db';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [db(), tailwind()],
+  output: 'server',
+  adapter: vercel(),
 });
